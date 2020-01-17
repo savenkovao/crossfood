286bf3c91fd77b212620d2c7c55eec673c6cf6f9
savenkovao <savienkov.ao@gmail.com>
2020-01-17 17:57:52 +0200

    privacy notice page added

---
+++ host/www.crossfood.od.ua/wp-content/themes/crossfood/page-privacy-notice.php
@@ -0,0 +1,20 @@
+<?php get_header('common') ?>
+<script>
+  window.CONFIG = {
+    page: 'single'
+  };
+</script>
+
+<section>
+  <div class="wrapper">
+    <div class="post-cont">
+      <h1 class="post-title"><?php the_title(); ?></h1>
+      <div style="overflow-x: auto">
+        <div style="min-width: 670px;">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<?php get_footer() ?>
